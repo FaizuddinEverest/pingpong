ff8f39ae087361c436b3c9cb25aaafcfa076622c
Faizuddin Mohammed <faizuddin@everest.engineering>
2022-06-22 18:38:46 +0530

    feat: second pairing round

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-export function hello(name) {
-  return `Hello ${name}`;
+const directionDeltas = {
+  N: {
+    f: [0, 1],
+    b: [0, -1],
+    r: [1, 0],
+    l: [-1, 0],
+  },
+  S: {
+    f: [0, -1],
+    b: [0, 1],
+    r: [1, 0],
+    l: [-1, 0],
+  },
+  E: {
+    r: [0, 1],
+    l: [0, -1],
+    f: [1, 0],
+    b: [-1, 0],
+  },
+  W: {
+    l: [0, 1],
+    r: [0, -1],
+    b: [1, 0],
+    f: [-1, 0],
+  },
+};
+
+const updateLocation = (current, delta) => [
+  current[0] + delta[0],
+  current[1] + delta[1],
+];
+
+function move(commands, calculatedDeltas, initialLocation) {
+  return commands
+    .split("")
+    .reduce(
+      (current, command) => updateLocation(current, calculatedDeltas[command]),
+      initialLocation
+    );
 }
+
+const rover = (initialLocation, initialDirection) => {
+  return (commands) =>
+    move(commands, directionDeltas[initialDirection], initialLocation);
+};
+
+export default rover;
--- index.test.js
+++ index.test.js
@@ -1,8 +1,47 @@
-import { hello } from "./index";
+import rover from "./index";
 
-describe("hello", () => {
-  it("should return the correct greeting", function () {
-    const result = hello("world");
-    expect(result).toBe("Hello world");
+describe("north", () => {
+  const initialDirection = "N";
+  const initialLocation = [0, 0];
+  const move = rover(initialLocation, initialDirection);
+
+  it("should return current location when no input", function () {
+    const result = move("");
+    expect(result).toEqual([0, 0]);
+  });
+
+  it('should move forward when input is "f"', function () {
+    const result = move("f");
+    expect(result).toEqual([0, 1]);
+  });
+
+  it('should move backward when input is "b"', function () {
+    const result = move("b");
+    expect(result).toEqual([0, -1]);
+  });
+
+  it('should not move when input is "fb"', function () {
+    const result = move("fb");
+    expect(result).toEqual([0, 0]);
+  });
+
+  it('should move 2 steps backward when input is "bb"', function () {
+    const result = move("bb");
+    expect(result).toEqual([0, -2]);
+  });
+
+  it('should move left side when input is "l"', function () {
+    const result = move("l");
+    expect(result).toEqual([-1, 0]);
+  });
+
+  it('should move right side when input is "r"', function () {
+    const result = move("r");
+    expect(result).toEqual([1, 0]);
+  });
+
+  it('should move right side when input is "r"', function () {
+    const result = move("r", "N");
+    expect(result).toEqual([1, 0]);
   });
 });
